5a72cad9e3ffa5f7bb3f5917ba4fb14aca90a3a3
azuse <misakaxindex@gmail.com>
2024-06-08 16:11:55 +0800

    add now playing metrics

--- jellyfin_exporter.py
+++ jellyfin_exporter.py
@@ -36,19 +36,63 @@ def request_api(action):
 
 class JellyfinCollector(object):
     def collect(self):
-        try:
-            sessions_data = request_api('/Sessions')
 
+            sessions_data = request_api('/Sessions')
+            import json
+            print(json.dumps(sessions_data))
             sessions_count = 0
             streams_count = 0
             streams_direct_count = 0
             streams_transcode_count = 0
             sessions = GaugeMetricFamily(
-                'jellyfin_active_users', 'Jellyfin active user sessions', labels=['user', 'client', 'device_name', 'jellyfin_instance'])
+                'jellyfin_active_users', 'Jellyfin active user sessions', 
+                labels=['user', 'client', 'device_name', 'play_name', 'path', 'run_time_ticks', 'container', 'video_display_title', 'bit_rate', 'bit_depth', 'color_space', 'audio_display_title', 'playing_position_ms', 
+                        "is_paused", "is_muted", "volume_level", "play_method", 'jellyfin_instance'])
             for user in sessions_data:
-                if user['IsActive'] == True:
-                    sessions.add_metric([user['UserName'], user['Client'], user['DeviceName'], API_BASEURL], 1)
-                    sessions_count += 1
+                if user.get("UserName", None) is None:
+                    continue
+
+                # NowPlayingItem
+                now_playing_item = user.get("NowPlayingItem", None)
+                now_playing_name = ""
+                path = ""
+                run_time_ticks = 0
+                container = ""
+                video_display_title = ""
+                bit_rate = 0
+                bit_depth = 0
+                color_space = ""
+                audio_display_title = ""
+                if now_playing_item is not None:
+                    now_playing_name = now_playing_item.get("Name", "")
+                    path = now_playing_item.get("Path", "")
+                    run_time_ticks = now_playing_item.get("RunTimeTicks", 0)
+                    container = now_playing_item.get("Container", "")
+                    if len(now_playing_item.get("MediaStreams", [])) > 1:
+                        video_display_title = now_playing_item.get("MediaStreams", [])[0].get("DisplayTitle", "")
+                        bit_rate = now_playing_item.get("MediaStreams", [])[0].get("BitRate", 0)
+                        bit_depth = now_playing_item.get("MediaStreams", [])[0].get("BitDepth", 0)
+                        color_space = now_playing_item.get("MediaStreams", [])[0].get("ColorSpace", "")
+                        audio_display_title = now_playing_item.get("MediaStreams", [])[1].get("DisplayTitle", "")
+                    
+                # PlayState
+                play_state = user.get("PlayState", None)
+                playing_position_ms = 0
+                is_paused = False
+                is_muted = False
+                volume_level = 0
+                play_method = ""
+                if play_state is not None:
+                    playing_position_ms = play_state.get("PositionTicks", 0)
+                    is_paused = play_state.get("IsPaused", False)
+                    is_muted = play_state.get("IsMuted", False)
+                    volume_level = play_state.get("VolumeLevel", 0)
+                    play_method = play_state.get("PlayMethod", "")
+
+
+
+                sessions.add_metric([user['UserName'], user['Client'], user['DeviceName'], now_playing_name, path, str(run_time_ticks), container, video_display_title, str(bit_rate), str(bit_depth), color_space, audio_display_title, str(playing_position_ms), str(is_paused), str(is_muted), str(volume_level), play_method, API_BASEURL], 1)
+                sessions_count += 1
 
                 if 'NowPlayingItem' in user:
                     streams_count += 1
@@ -93,8 +137,7 @@ class JellyfinCollector(object):
                 items_counts.add_metric([metric, API_BASEURL], val)
             yield items_counts
 
-        except Exception as ex:
-            logging.error('Error getting metrics: %s', ex)
+
 
 REGISTRY.register(JellyfinCollector())
 start_http_server(PORT)
